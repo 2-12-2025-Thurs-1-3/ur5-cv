f13a4d9e6efa2604d93e68f7ea53b5382d816d44
EduardoGarcia1 <egarcia4632@gmail.com>
2025-04-24 14:35:56 -0400

    Update CoMTest.py

--- CoMTest.py
+++ CoMTest.py
@@ -5,11 +5,28 @@ import time
 from tkinter import *
 import math
 
+COLORS = [(0,255,120),(255,120,120),(0,69,255),(255,255,255)]
+
+def find_contours(image,original,colors):
+    contoursss, _ = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    min_area = 2500
+    contourss = [cnt for cnt in contoursss if (cv2.contourArea(cnt) > min_area and cv2.contourArea(cnt) < 9000)]
+    cv2.drawContours(original, contourss, -1, colors,3)
+    if (len(contourss)>0):
+        for c in contourss:
+            # Compute the center of the contour
+            M = cv2.moments(c)
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            # Draw the contour and center of the shape on the image
+            cv2.circle(original, (cX, cY), 7, (255, 255, 255), -1)
+            cv2.putText(original, "center", (cX - 20, cY - 20),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
 def main():
     # Open up the webcam
     cap = cv2.VideoCapture(2)
     while True:
-        #tk.update()
 
         # Read from the camera frame by frame and crop
         ret, cv_image1 = cap.read()
@@ -24,11 +41,11 @@ def main():
         lower_bound_HSV_blue = np.array([71, 135, 68])
         upper_bound_HSV_blue = np.array([105, 255, 255])
         # orange 
-        lower_bound_HSV_orange = np.array([15, 124, 255])
+        lower_bound_HSV_orange = np.array([15, 100, 255])
         upper_bound_HSV_orange = np.array([23, 255, 255])
         # clear
-        lower_bound_HSV_clear = np.array([86, 15, 83])
-        upper_bound_HSV_clear = np.array([203, 150, 255])
+        lower_bound_HSV_clear = np.array([0, 0, 100])
+        upper_bound_HSV_clear = np.array([180, 60, 255])
         
         # create four color masks
         mask_HSV_yellow = cv2.inRange(hsv_image, lower_bound_HSV_yellow, upper_bound_HSV_yellow)
@@ -45,22 +62,9 @@ def main():
         opening_orange = cv2.morphologyEx(mask_HSV_orange, cv2.MORPH_OPEN, kernel, iterations = num_iterations)
         opening_clear = cv2.morphologyEx(mask_HSV_clear, cv2.MORPH_OPEN, kernel, iterations = num_iterations)
 
-        #ret, thresh = cv2.threshold(test, 150, 255, cv2.THRESH_BINARY)
-        contoursss, _ = cv2.findContours(openIm, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        min_area = 2500
-        contourss = [cnt for cnt in contoursss if (cv2.contourArea(cnt) > min_area and cv2.contourArea(cnt) < 6000)]
-        cv2.drawContours(cv_image, contourss, -1, (0, 255, 0),3)
-        for c in contourss:
-            # Compute the center of the contour
-            M = cv2.moments(c)
-            cX = int(M["m10"] / M["m00"])
-            cY = int(M["m01"] / M["m00"])
-
-            # Draw the contour and center of the shape on the image
-            cv2.drawContours(cv_image, [c], -1, (0, 255, 0), 2)
-            cv2.circle(cv_image, (cX, cY), 7, (255, 255, 255), -1)
-            cv2.putText(cv_image, "center", (cX - 20, cY - 20),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        images = [opening_yellow,opening_blue,opening_orange,opening_clear]
+        for img in range(len(images)):
+            find_contours(images[img],cv_image,COLORS[img])
 
         ## display image
         cv2.imshow("Original",cv_image)
@@ -68,7 +72,7 @@ def main():
         cv2.imshow("Opening - Blue", opening_blue)
         cv2.imshow("Opening - Orange", opening_orange)
         cv2.imshow("Opening - Clear", opening_clear)
-        print(cv_image.shape)
+        #print(cv_image.shape)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
