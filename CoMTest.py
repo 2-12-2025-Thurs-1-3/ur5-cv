1512d936f414212613e0dab86bedd98faeccc382
ivagoel <ivagoel@mit.edu>
2025-04-24 14:12:12 -0400

    Update CoMTest.py
    added 4 color masks

--- CoMTest.py
+++ CoMTest.py
@@ -16,15 +16,34 @@ def main():
         cv_image = cv_image1[0:440,138:540]
         hsv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
 
-        # get threshold values
-        lower_bound_yellow = np.array([23,41,105])
-        upper_bound_yellow = np.array([34,255,255])
-        mask_HSV = cv2.inRange(hsv_image, lower_bound_yellow, upper_bound_yellow)
+        # threshold values
+        # yellow 
+        lower_bound_HSV_yellow = np.array([23, 41, 105]) 
+        upper_bound_HSV_yellow = np.array([34, 255, 255])
+        # blue 
+        lower_bound_HSV_blue = np.array([71, 135, 68])
+        upper_bound_HSV_blue = np.array([105, 255, 255])
+        # orange 
+        lower_bound_HSV_orange = np.array([15, 124, 255])
+        upper_bound_HSV_orange = np.array([23, 255, 255])
+        # clear
+        lower_bound_HSV_clear = np.array([86, 15, 83])
+        upper_bound_HSV_clear = np.array([203, 150, 255])
+        
+        # create four color masks
+        mask_HSV_yellow = cv2.inRange(hsv_image, lower_bound_HSV_yellow, upper_bound_HSV_yellow)
+        mask_HSV_blue = cv2.inRange(hsv_image, lower_bound_HSV_blue, upper_bound_HSV_blue)
+        mask_HSV_orange = cv2.inRange(hsv_image, lower_bound_HSV_orange, upper_bound_HSV_orange)
+        mask_HSV_clear = cv2.inRange(hsv_image, lower_bound_HSV_clear, upper_bound_HSV_clear)
+        
         kernel = np.ones((7,7),np.uint8)
+        num_iterations = 3
 
         ################ Opening ####################
-        test = mask_HSV.copy()
-        openIm = cv2.morphologyEx(test, cv2.MORPH_OPEN, kernel)
+        opening_yellow = cv2.morphologyEx(mask_HSV_yellow, cv2.MORPH_OPEN, kernel, iterations = num_iterations)
+        opening_blue = cv2.morphologyEx(mask_HSV_blue, cv2.MORPH_OPEN, kernel, iterations = num_iterations)
+        opening_orange = cv2.morphologyEx(mask_HSV_orange, cv2.MORPH_OPEN, kernel, iterations = num_iterations)
+        opening_clear = cv2.morphologyEx(mask_HSV_clear, cv2.MORPH_OPEN, kernel, iterations = num_iterations)
 
         #ret, thresh = cv2.threshold(test, 150, 255, cv2.THRESH_BINARY)
         contoursss, _ = cv2.findContours(openIm, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
@@ -45,7 +64,10 @@ def main():
 
         ## display image
         cv2.imshow("Original",cv_image)
-        cv2.imshow("Opening", openIm)
+        cv2.imshow("Opening - Yellow", opening_yellow)
+        cv2.imshow("Opening - Blue", opening_blue)
+        cv2.imshow("Opening - Orange", opening_orange)
+        cv2.imshow("Opening - Clear", opening_clear)
         print(cv_image.shape)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
