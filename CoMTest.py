7bb072294ca41473314c25731f33ed726c7bec20
Seth <sethrobles04@gmail.com>
2025-04-29 19:08:12 -0400

    Added changes that track one bottle's centroid based using the class Eduardo made

--- CoMTest.py
+++ CoMTest.py
@@ -5,12 +5,16 @@ import time
 from tkinter import *
 import math
 
+bottles = []
 colorID = [0,0,0,0]
 COLORS = [(0,255,120),(255,120,120),(0,69,255),(255,255,255)]
 POINTS = [(.69154,-.600138),(.67307,-.135855),(.1763488,-.1238),(.13237,-.63746)]
 MYPOINTS = [(33,403),(350,400),(370,67),(25,25)]
 src = np.array(MYPOINTS)
 dst = np.array(POINTS)
+
+cameraPort = 8 ## Which port you're working with. Set!!!
+
 
 x, y, u, v = src[:,0], src[:,1], dst[:,0], dst[:,1]
 A = np.zeros((9,9))
@@ -26,17 +30,21 @@ H = np.reshape(np.linalg.solve(A, b), (3,3))
 print(H)
 
 class Bottle:
-    def __init__(self,clr: list, pos: list):
+    def __init__(self,clr: list, pos: list, status: bool):
         self.color = clr
         self.pos = [pos]
-        self.time = [time.time_ns()]
+        self.time = [time.time()]
         self.interpolate = 0
+        self.status = False
     def update_pos(self,new_pos: list):
         self.pos.append(new_pos)
-        self.time.append(time.time_ns())
+        self.time.append(time.time())
     def find_velocity(self):
         self.velX = (self.pos[-1][0]-self.pos[0][0])/(self.time[-1]-self.time[0])
         self.velY = (self.pos[-1][1]-self.pos[0][1])/(self.time[-1]-self.time[0])
+        return self.velX, self.velY
+    def update_status(self)
+        self.status = True
 
 def find_contours(i,image,original,colors):
     contoursss, _ = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
@@ -48,31 +56,110 @@ def find_contours(i,image,original,colors):
         for c in contourss:
             # Compute the center of the contour
             M = cv2.moments(c)
+            if M["m00"] == 0:
+                continue  # skip this contour
             cX = int(M["m10"] / M["m00"])
             cY = int(M["m01"] / M["m00"])
-            # Draw the contour and center of the shape on the image
+
+
+
+
+            # # Draw the contour and center of the shape on the image
+            # cv2.circle(original, (cX, cY), 7, (255, 255, 255), -1)
+            # cv2.putText(original, "center", (cX - 20, cY - 20),
+            #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            # print("Pos: ",(cX,cY))
+            # new = [H[0,0]*cX+H[0,1]*cY+H[0,2],H[1,0]*cX+H[1,1]*cY+H[1,2]]
+            # print("New Pos: ",new)
+
+            pos = [cX, cY]
+            if len(bottles) <= i or bottles[i] is None:
+                while len(bottles) <= i:
+                    bottles.append(None)
+                bottles[i] = Bottle(colors, pos)
+            else:
+                bottles[i].update_pos(pos)
+
             cv2.circle(original, (cX, cY), 7, (255, 255, 255), -1)
             cv2.putText(original, "center", (cX - 20, cY - 20),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-            print("Pos: ",(cX,cY))
-            new = [H[0,0]*cX+H[0,1]*cY+H[0,2],H[1,0]*cX+H[1,1]*cY+H[1,2]]
-            print("New Pos: ",new)
+            print("Pos: ", (cX, cY))
+            new = [H[0, 0]*cX + H[0, 1]*cY + H[0, 2], H[1, 0]*cX + H[1, 1]*cY + H[1, 2]]
+            print("New Pos: ", new)
+
+            if len(bottles[i].pos) >= 2:
+                vx, vy = bottles[i].find_velocity()
+                print(f"Velocity (px/ns): ({vx:.2e}, {vy:.2e})")
+
+
+# Draws points where a bottle has been
+def draw_bottle_trajectory(image, bottle: Bottle):
+    if len(bottle.pos) < 2:
+        return
+    for i in range(1, len(bottle.pos)):
+        pt1 = tuple(map(int, bottle.pos[i - 1]))
+        pt2 = tuple(map(int, bottle.pos[i]))
+        cv2.line(image, pt1, pt2, bottle.color, 2)
+
+
+
+# Allows us to just track based on a single image. We may have to fine-tune the colors, but if we pair this with the class, then we should have an easier time tracking a centroid
+def track_single_color(color_name, hsv_image, original_image):
+    global bottles  # Make sure we're modifying the shared list
+
+    bounds = {
+        "yellow": (np.array([21, 39, 103]), np.array([36, 255, 255]), 0),
+        "blue":   (np.array([71, 135, 68]), np.array([105, 255, 255]), 1),
+        "orange": (np.array([13, 90, 200]), np.array([25, 255, 255]), 2),
+        "clear":  (None, None, 3)
+    }
+
+    if color_name.lower() not in bounds:
+        print(f"[ERROR] Color '{color_name}' not supported.")
+        return
+
+    lower, upper, idx = bounds[color_name.lower()]
+
+    if lower is not None:
+        mask = cv2.inRange(hsv_image, lower, upper)
+        kernel = np.ones((7, 7), np.uint8)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=3)
+
+        find_contours(idx, mask, original_image, COLORS[idx])
+        cv2.imshow(f"Tracking: {color_name}", mask)
+
+        # Draw the trajectory for this bottle if it exists
+        if len(bottles) > idx and bottles[idx] is not None:
+            draw_bottle_trajectory(original_image, bottles[idx])
+
+
+
+
 
 def main():
     # Open up the webcam
-    cap = cv2.VideoCapture(2)
+    cap = cv2.VideoCapture(cameraPort) # Change this for your port
+    if not cap.isOpened():
+        print("Error: Camera not grabbing data.")
+        exit()
+
     while True:
 
         # Read from the camera frame by frame and crop
-        ret, cv_image1 = cap.read()
-        cv_image = cv_image1[0:440,138:540]
+        ret, frame = cap.read()
+        if not ret:
+            print("Failed to grab frame")
+            break
+
+        cv_image = frame[0:440,138:540]
+
         #print(len(cv_image))
         hsv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
 
         # threshold values
         # yellow 
-        lower_bound_HSV_yellow = np.array([23, 41, 105]) 
-        upper_bound_HSV_yellow = np.array([34, 255, 255])
+        # lower_bound_HSV_yellow = np.array([23, 41, 105]) 
+        # upper_bound_HSV_yellow = np.array([34, 255, 255])
         lower_bound_HSV_yellow = np.array([21, 39, 103]) 
         upper_bound_HSV_yellow = np.array([36, 255, 255])
         # blue 
@@ -86,7 +173,6 @@ def main():
         # clear
         # lower_bound_HSV_clear = np.array([255, 255, 255])
         # upper_bound_HSV_clear = np.array([255, 255, 255])
-        ret, frame = cap.read()
         cannyIm = cv2.Canny(cv_image, 50, 150, apertureSize = 3)
         cv2.imshow("Canny_Image", cannyIm)
         cv2.waitKey(3)
@@ -109,17 +195,21 @@ def main():
         opening_clear = cv2.morphologyEx(cannyIm, cv2.MORPH_CLOSE, kernel, iterations = num_iterations)
 
         images = [opening_yellow,opening_blue,opening_orange,opening_clear]
-        colorID.clear()
-        for img in range(len(images)):
-            find_contours(img,images[img],cv_image,COLORS[img])
-        if colorID[0]==1:
-            print("yellow")
-        elif colorID[1]==1:
-            print("blue")
-        elif colorID[2]==1:
-            print("orange")
-        elif colorID[3]==1:
-            print("clear")
+        # colorID = [0,0,0,0]
+        # for img in range(len(images)):
+        #     find_contours(img,images[img],cv_image,COLORS[img])
+        # if colorID[0]==1:
+        #     print("yellow")
+        # elif colorID[1]==1:
+        #     print("blue")
+        # elif colorID[2]==1:
+        #     print("orange")
+        # elif colorID[3]==1:
+        #     print("clear")
+
+
+        colorID = [0,0,0,0]
+        track_single_color("yellow", hsv_image, cv_image)
 
         #cv2.circle(cv_image, (33,403), 7, (255, 255, 255), -1)
         #cv2.circle(cv_image, (350,400), 7, (255, 255, 255), -1)
@@ -128,10 +218,10 @@ def main():
         
         ## display image
         cv2.imshow("Original",cv_image)
-        cv2.imshow("Opening - Yellow", opening_yellow)
-        cv2.imshow("Opening - Blue", opening_blue)
-        cv2.imshow("Opening - Orange", opening_orange)
-        cv2.imshow("Opening - Clear", opening_clear)
+        # cv2.imshow("Opening - Yellow", opening_yellow)
+        # cv2.imshow("Opening - Blue", opening_blue)
+        # cv2.imshow("Opening - Orange", opening_orange)
+        # cv2.imshow("Opening - Clear", opening_clear)
         #print(cv_image.shape)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
